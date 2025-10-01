9c885ff162d1b8dbc8ac15c891069952f3631204
yvetter438 <yannick.vetter@outlook.com>
2025-10-01 14:52:13 -0700

    added privacy policy and terms of service to site

--- src/routes/pages.js
+++ src/routes/pages.js
@@ -14,4 +14,12 @@ router.get('/dashboard', ensureAuthenticated, (req, res) => {
 	res.render('dashboard', { title: 'Task Matrix' });
 });
 
+router.get('/privacy', (req, res) => {
+	res.render('privacy', { title: 'Privacy Policy' });
+});
+
+router.get('/terms', (req, res) => {
+	res.render('terms', { title: 'Terms of Service' });
+});
+
 module.exports = router;
